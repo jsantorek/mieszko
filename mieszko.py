3a70f4d644d9ba30ac61685e66c5a6c887ac4bfd
jsantorek <jakub.santorek@gmail.com>
2017-12-05 00:18:31 +0100

    sources added, started I2C accelerometer driver

--- mieszko.py
+++ mieszko.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import driver.pololu_minIMU as imu
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT, initial=GPIO.HIGH)
@@ -14,3 +15,8 @@ time.sleep(1)
 GPIO.output(25, GPIO.LOW)
 
 GPIO.cleanup()
+
+imu.init()
+for i in range(1000):
+    print(imu.read_acc())
+
