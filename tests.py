8d27b03ed04aaed29d51678f98a7b9ef1a9bde5a
jsantorek <jakub.santorek@gmail.com>
2017-12-30 23:42:10 +0100

    fixed whiskers tests array usage

--- tests.py
+++ tests.py
@@ -54,41 +54,41 @@ def sharp_test():
     for i in range(10):
         v.append(shp.whiskers.whisker1.read())
         time.sleep(0.1)
-    print("Whisker1 reading: {}".format(np.mean(np.ndarray(v, dtype=np.int8))))
+    print("Whisker1 reading: {}".format(np.mean(np.ndarray(v))))
 
     v = []
     print("Reading Whisker2 for 1 second")
     for i in range(10):
         v.append(shp.whiskers.whisker2.read())
         time.sleep(0.1)
-    print("Whisker1 reading: {}".format(np.mean(np.ndarray(v, dtype=np.int8))))
+    print("Whisker2 reading: {}".format(np.mean(np.ndarray(v))))
 
     v = []
     print("Reading Whisker3 for 1 second")
     for i in range(10):
         v.append(shp.whiskers.whisker3.read())
         time.sleep(0.1)
-    print("Whisker1 reading: {}".format(np.mean(np.ndarray(v, dtype=np.int8))))
+    print("Whisker3 reading: {}".format(np.mean(np.ndarray(v))))
 
     v = []
     print("Reading Whisker4 for 1 second")
     for i in range(10):
         v.append(shp.whiskers.whisker4.read())
         time.sleep(0.1)
-    print("Whisker1 reading: {}".format(np.mean(np.ndarray(v, dtype=np.int8))))
+    print("Whisker4 reading: {}".format(np.mean(np.array(v))))
 
     v = []
     print("Reading Whisker5 for 1 second")
     for i in range(10):
         v.append(shp.whiskers.whisker5.read())
         time.sleep(0.1)
-    print("Whisker1 reading: {}".format(np.mean(np.ndarray(v, dtype=np.int8))))
+    print("Whisker5 reading: {}".format(np.mean(np.array(v))))
 
     v = []
     print("Reading all whiskers for 1 second")
     for i in range(10):
         v.append(shp.whiskers.read())
         time.sleep(0.1)
-    print("Whisker1 reading: " + str(np.mean(np.ndarray(v, dtype=np.int8), axis=1).tolist()))
+    print("Whisker1 reading: " + str(np.mean(np.array(v), axis=1).tolist()))
 
     print("Sharp tests finished")
